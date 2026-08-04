18884027c01d3db913837eb84cba847a0ef42b87
linh0804 <linhlinh1328495@gmail.com>
2026-08-05 00:09:05 +0700

    Remove vendor from git

--- vendor/nightmare/php/src/Http/Client.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Nightmare\Http;
-
-use Symfony\Component\HttpClient\CurlHttpClient;
-
-class_alias(CurlHttpClient::class, 'Nightmare\Http\Client');
--- vendor/nightmare/php/src/Http/ClientNative.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Nightmare\Http;
-
-use Symfony\Component\HttpClient\HttpClient;
-
-class_alias(HttpClient::class, 'Nightmare\Http\ClientNative');
